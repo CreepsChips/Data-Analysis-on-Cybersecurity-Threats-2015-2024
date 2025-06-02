9f1b36590890c2186c3006ba1bfd0f6fc1b61576
CreepsChips <99000641+CreepsChips@users.noreply.github.com>
2025-06-02 20:25:40 +0800

    Update total_attack_type.sql

--- total_attack_type.sql
+++ total_attack_type.sql
@@ -1,6 +1,8 @@
+-- Selecting the final query which selects the number of attacks per attack type
 SELECT 
 	attack_type,
     COUNT(*) AS total_attack_type
+-- Subquery that covers the attack type between 2015 and 2024
 FROM (SELECT year, attack_type
 	  FROM global_cybersecurity_threats_2015_2024
       WHERE year BETWEEN 2015 AND 2024) AS filtered_case
