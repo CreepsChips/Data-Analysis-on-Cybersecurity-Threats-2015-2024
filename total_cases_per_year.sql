fee974dc0d162b73a1df83dc29d8749e8541434c
CreepsChips <99000641+CreepsChips@users.noreply.github.com>
2025-06-02 20:29:13 +0800

    Update total_cases_per_year.sql

--- total_cases_per_year.sql
+++ total_cases_per_year.sql
@@ -1,7 +1,14 @@
+-- Total cases of cybersecurity attacks per year
+-- Purpose: to determine what year had the highest cases for all countries
+-- Dataset: Global_Cybersecurity_Threats_2015_2024.csv
+-- By CreepsChips
+
+-- Select statement where total cases per year had been counted
 SELECT
 	year,
     COUNT(*) AS cases
 FROM global_cybersecurity_threats_2015_2024
+-- Determine what year covered by the query
 WHERE (year BETWEEN 2015 AND 2024)
 GROUP BY year
 ORDER BY year DESC;
