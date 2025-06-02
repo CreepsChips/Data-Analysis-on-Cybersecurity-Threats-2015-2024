de4f04c10d878bb5b54e2753e9935ae019418cbc
CreepsChips <99000641+CreepsChips@users.noreply.github.com>
2025-06-02 20:31:32 +0800

    Update total_loss_per_country.sql

--- total_loss_per_country.sql
+++ total_loss_per_country.sql
@@ -1,3 +1,8 @@
+-- Total financial loss for every country
+-- Purpose: to determine what countries have the highest financial loss due to cybersecurity attacks
+-- Dataset: Global_Cybersecurity_Threats_2015_2024.csv
+-- By CreepsChips
+
 SELECT 
 	country,
     ROUND(SUM(financial_loss_millions),2) AS total_loss_millions
