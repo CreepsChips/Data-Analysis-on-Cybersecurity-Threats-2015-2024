64ce5aa3cfcfe7bfb95901b508d5520fdcbef4da
CreepsChips <99000641+CreepsChips@users.noreply.github.com>
2025-06-03 19:40:05 +0800

    Update total_loss_per_year.sql

--- total_loss_per_year.sql
+++ total_loss_per_year.sql
@@ -6,7 +6,7 @@
 SELECT 
 	year,
     ROUND(SUM(financial_loss_millions),2) AS total_loss_millions,
-    RANK() OVER(ORDER BY SUM(financial_loss_millions) ASC) AS loss_rank
+    RANK() OVER(ORDER BY SUM(financial_loss_millions) DESC) AS loss_rank
 FROM (SELECT year, financial_loss_millions
        FROM global_cybersecurity_threats_2015_2024) AS filtered_case
 GROUP BY year
