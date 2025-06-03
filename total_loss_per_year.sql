d37669920eedf081ce924c5f0c9bae3a104dcbd8
CreepsChips <99000641+CreepsChips@users.noreply.github.com>
2025-06-03 19:38:34 +0800

    Update total_loss_per_year.sql

--- total_loss_per_year.sql
+++ total_loss_per_year.sql
@@ -1,7 +1,13 @@
+-- Total financial loss for every year
+-- Purpose: to determine what year have the highest financial loss due to cybersecurity attacks
+-- Dataset: Global_Cybersecurity_Threats_2015_2024.csv
+-- By CreepsChips
+
 SELECT 
 	year,
-    ROUND(SUM(financial_loss_millions),2) AS total_loss_millions
+    ROUND(SUM(financial_loss_millions),2) AS total_loss_millions,
+    RANK() OVER(ORDER BY SUM(financial_loss_millions) ASC) AS loss_rank
 FROM (SELECT year, financial_loss_millions
        FROM global_cybersecurity_threats_2015_2024) AS filtered_case
 GROUP BY year
-ORDER BY year DESC;
+ORDER BY loss_rank ASC;
